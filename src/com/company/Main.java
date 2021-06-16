6712ad808231ae6821bada6ab4ab8b63f3fa5565
JsEaVfje <anderss1918@gmail.com>
2021-06-16 14:45:52 +0200

    Origin

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String number = scanner.nextLine();
+        if (number.charAt(0) == '0') {
+            System.out.println("Not Duck number");
+        } else if (number.contains("0")) {
+            System.out.println("Duck number");
+        } else {
+            System.out.println("Not Duck number");
+        }
+    }
+}
